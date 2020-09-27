627ad788676d350dc92c6d3a78b2bba48777bfd7
asadambiseek <71602282+asadambiseek@users.noreply.github.com>
2020-09-27 09:14:18 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,5 +7,24 @@ $(document).ready(function() {
     $('.overlay').click(function() {
         $('.links, .overlay').removeClass('display');
     })
+});
 
-})
+    $(function(){
+        $(".ajaxForm").submit(function(e){
+            e.preventDefault();
+            var href = $(this).attr("action");
+            $.ajax({
+                type: "POST",
+                dataType: "json",
+                url: href,
+                data: $(this).serialize(),
+                success: function(response){
+                    if(response.status == "success"){
+                        window.location.href = 'thanks'; // change this. 
+                    }else{
+                        alert("An error occured: " + response.message);
+                    }
+                }
+            });
+        });
+    });
